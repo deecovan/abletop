57fa98413735c0ee051ecb5cfbf027ad2108bcd0
Dee <dee.covan@gmail.com>
2025-06-12 00:30:47 +0300

    Create proto Card and try to draw one random

---
+++ guide/Scripts/CardDatabase.gd
@@ -0,0 +1,5 @@
+const CARDS = {
+	"Knight": [2, 3],
+	"Archer": [1, 1],
+	"Daemon": [5, 7],
+	}
--- guide/Scripts/Deck.gd
+++ guide/Scripts/Deck.gd
@@ -3,13 +3,15 @@ extends Node2D
 const CARD_DRAW_SPEED = 0.2
 
 var card_scene = preload("res://Scenes/Card.tscn")
-var player_deck = ["Knight", "Knight", "Knight"]
+var card_database_reference = preload("res://Scripts/CardDatabase.gd")
+var player_deck = ["Knight", "Archer", "Daemon", "Knight"]
 
 func _ready() -> void:
-	$RichTextLabel.text = str(player_deck.size())
 	draw_card()
+	$RichTextLabel.text = str(player_deck.size())
 	
 func draw_card() -> void:
+	player_deck.shuffle()
 	var card_drawn = player_deck[0]
 	player_deck.erase(card_drawn)
 	if player_deck.size() == 0:
@@ -19,6 +21,8 @@ func draw_card() -> void:
 		
 	$RichTextLabel.text = str(player_deck.size())
 	var new_card = card_scene.instantiate()
-	$"../CardManager".add_child(new_card)
 	new_card.name = "Card"
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn][0])
+	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn][1])
+	$"../CardManager".add_child(new_card)
 	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
