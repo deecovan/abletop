7dc17c490f6b331f73872cf926e64cf4932e261d
Dee <dee.covan@gmail.com>
2025-06-11 23:44:08 +0300

    Proto Deck and empty PlayerHand

--- guide/Scripts/Deck.gd
+++ guide/Scripts/Deck.gd
@@ -1,15 +1,24 @@
 extends Node2D
 
-const CARD_SCENE_PATH = "res://Scenes/Card.tscn"
 const CARD_DRAW_SPEED = 0.2
 
+var card_scene = preload("res://Scenes/Card.tscn")
 var player_deck = ["Knight", "Knight", "Knight"]
 
 func _ready() -> void:
 	$RichTextLabel.text = str(player_deck.size())
+	draw_card()
 	
 func draw_card() -> void:
 	var card_drawn = player_deck[0]
 	player_deck.erase(card_drawn)
 	if player_deck.size() == 0:
-		pass
+		$Area2D/CollisionShape2D.disabled = true
+		$Sprite2D.visible = false
+		$RichTextLabel.visible = false
+		
+	$RichTextLabel.text = str(player_deck.size())
+	var new_card = card_scene.instantiate()
+	$"../CardManager".add_child(new_card)
+	new_card.name = "Card"
+	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
---
+++ guide/Scripts/PlayerHand.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func add_card_to_hand(_card, _speed) -> void:
+	print(_card, _speed)
