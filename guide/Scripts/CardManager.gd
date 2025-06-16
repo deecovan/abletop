c6bd2bc516d7451b07768922f54aef7a23ce7246
Dee <dee.covan@gmail.com>
2025-06-16 08:13:43 +0300

    Add CardSlot and script 05:46 Video#3

--- guide/Scripts/CardManager.gd
+++ guide/Scripts/CardManager.gd
@@ -33,6 +33,7 @@ func start_drag(card) -> void:
 func finish_drag() -> void:
 	card_being_dragged.scale = Vector2(1,1)
 	var card_slot_found = raycast_check_for_card_slot()
+	print(card_slot_found)
 	if card_slot_found and not card_slot_found.card_in_slot:
 		player_hand_reference.remove_card_from_hand(card_being_dragged)
 		card_being_dragged.position = card_slot_found.position
