f82e978acc51428a1103cb1b2104b43fa84ffb3b
Dee <dee.covan@gmail.com>
2025-07-02 00:37:54 +0300

    Prototype functions try_play_card() end_opponent_turn()

--- guide/Scripts/BattleManager.gd
+++ guide/Scripts/BattleManager.gd
@@ -6,6 +6,16 @@ var opponent_deck
 
 func _ready() -> void:
 	opponent_deck = $"../OpponentDeck"
+	empty_monster_card_slots = [
+		$"../OpponentCardSlots/CardSlot", 
+		$"../OpponentCardSlots/CardSlot2",
+		$"../OpponentCardSlots/CardSlot3",
+		$"../OpponentCardSlots/CardSlot4",
+		$"../OpponentCardSlots/CardSlot5",
+		$"../OpponentCardSlots/CardSlot6",
+		$"../OpponentCardSlots/CardSlot7",
+		$"../OpponentCardSlots/CardSlot8"
+	]
 
 func _on_end_turn_button_pressed() -> void:
 	opponent_turn()
@@ -13,19 +23,35 @@ func _on_end_turn_button_pressed() -> void:
 func opponent_turn() -> void:
 	$EndTurnButton.disabled = true
 	$EndTurnButton.visible = false
-	await battle_timer(500.0 * randf())
+	await battle_timer()
 	
 	# Implement turn
 	## Draw a card
 	if opponent_deck.opponent_deck.size() > 0:
 		opponent_deck.draw_card()
-		await battle_timer(500.0 * randf())
+		await battle_timer()
 	
 	if opponent_deck.opponent_deck.size() > 0:
 		$EndTurnButton.disabled = false
 		$EndTurnButton.visible = true
+		
+	if empty_monster_card_slots.size() == 0:
+		end_opponent_turn()
+		return
+	
+	await try_play_card()
+	end_opponent_turn()
+	
+func  try_play_card():
+	print("try_play_card()")
+	return battle_timer()
 
-func battle_timer(delay = 1000.0):
+func end_opponent_turn() -> void:
+	print("end_opponent_turn()")
+	
+
+
+func battle_timer(delay = 500.0 * randf()):
 	$BattleTimer.wait_time = delay / 1000
 	$BattleTimer.start()
 	return $BattleTimer.timeout
--- guide/Scripts/Main.gd
+++ guide/Scripts/Main.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+func _ready() -> void:
+	Engine.max_fps = 30
+	Engine.physics_ticks_per_second = 30
+
 func _process(_delta):
 	if Input.is_action_pressed('reload'):
 		get_tree().reload_current_scene()
